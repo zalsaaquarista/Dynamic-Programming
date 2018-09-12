32235f6475bbe1c43068294c25a089ef34500b32
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-09-12 12:42:38 +0700

    Initial Commit

---
+++ src/Fibonacci/Contoh1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fibonacci;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.Scanner;
+public class Contoh1 {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas: " + identitas);
+       
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    
+    
+}
