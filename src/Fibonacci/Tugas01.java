bb8f59df4185e269be25347d956a69155b668352
ASUS <ASUS@LAPTOP-48GGUI0O>
2018-09-14 20:26:22 +0700

    While jumlah uang 5000

---
+++ src/Fibonacci/Tugas01.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fibonacci;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.Scanner;
+public class Tugas01 {
+    public static void main(String[] args)
+    {
+        Scanner baca = new Scanner(System.in);
+        
+        System.out.print("Masukkan jumlah uang: ");
+        int uang = baca.nextInt();
+        int[] data = new int[] {100, 500, 1000, 2000, 5000};
+        
+        int a = 0;
+        while(uang >= 5000)
+        {
+            uang = uang - 5000;
+            a++;
+        }
+        System.out.println("Banyak uang 5000: " + a);
+        
+}
